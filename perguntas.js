2d7d52a8204e6cf07c73229ff95a2e0407f38c96
rodriguesgab488 <gabrielgaalves8@gmail.com>
2024-11-21 19:56:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Moto',
+    'Qual é a moto mais rápida do mundo?',
+    'Kawasaki Ninja H2R'
+)
+
+criaCartao(
+    'Carro',
+    'Qual o carro com o ronco mais fino e lindo do mundo?',
+    'Lamborghini Aventador SVJ com o seu motor V12'
+)
+
+criaCartao(
+    'Avião',
+    'Qual é o maior Avião do mundo?',
+    'Airbus A380'
+)
+
+criaCartao(
+    'Velocidade',
+    'Qual foi a maior velocidade, que uma aeronave alcançou?',
+    '12.348 km/h X43'
+)
